2f6288f79a4db393c541a32db1a0866d79451a61
Anurag Prajapati <prajapatianurag73240@gmail.com>
2025-03-12 12:42:40 +0530

    Added Invite member button in our organization ,added button in nvabar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,19 +1,46 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationProfile, OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import React from "react";
+import { SearchInput } from "./search-intput";
+
+import { InviteButton } from "./invite-button";
 
 const navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className=" bg-yellow-500 flex-1">
+    <div className="flex items-center gap-x-4 p-5 ">
+      <div className=" hidden lg:flex lg:flex-1">
         {/*todo: search*/}
-        Search
+        <SearchInput />
       </div>
 
-      <div>
-        PI
-        <UserButton />
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItme: "center",
+                width: "100%",
+                maxWidth:"376px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #E5E7EB",
+                justifyContent: "space-between",
+                backgroundColor: "white",
+              },
+            },
+          }}
+        />
       </div>
+      <OrganizationProfile/>
+
+      <InviteButton/>
+      <UserButton />
     </div>
   );
 };
