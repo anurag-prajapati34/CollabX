dc9131c17d7ff7356b0139509fcb893359d7e472
Anurag Prajapati <prajapatianurag73240@gmail.com>
2025-04-30 13:49:55 +0530

    feat: structured board list

--- app/(dashboard)/_components/borad-card/index.tsx
+++ app/(dashboard)/_components/borad-card/index.tsx
@@ -4,6 +4,11 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Overlay } from "./overlay";
+import { formatDistanceToNow } from "date-fns";
+import { useAuth } from "@clerk/nextjs";
+import { Footer } from "./footer";
+import { Skeleton } from "@/components/ui/skeleton";
+
 interface BoardCardProps {
   id: string;
   title: string;
@@ -25,6 +30,11 @@ const BoardCard = ({
   isFavorite,
   orgId,
 }: BoardCardProps) => {
+  const { userId } = useAuth();
+  const authorLabel = userId === authorId ? "You" : authorName;
+  const createdAtLabel = formatDistanceToNow(createdAt, {
+    addSuffix: true,
+  });
   return (
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
@@ -32,9 +42,24 @@ const BoardCard = ({
           <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
         </div>
+        <Footer
+          isFavorite={isFavorite}
+          title={title}
+          authorLabel={authorLabel}
+          createdAtLabel={createdAtLabel}
+          onClick={() => {}}
+          disabled={false}
+        />
       </div>
     </Link>
   );
 };
 
+BoardCard.Skeleton = function BoardCardSkeleton() {
+  return (
+    <div className="group aspect-[100/127] border rounded-lg  overflow-hidden">
+      <Skeleton className="h-full w-full" />
+    </div>
+  );
+};
 export default BoardCard;
