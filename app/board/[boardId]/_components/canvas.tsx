97f5f896a98490d3517110cd5962aadd78b6bdc6
Anurag Prajapati <prajapatianurag73240@gmail.com>
2025-05-09 09:41:23 +0530

    feat : add unselect functionallity whenever click on empty place

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
@@ -23,7 +23,11 @@ import {
   useStorage,
 } from "@liveblocks/react/suspense";
 import { CursorsPresence } from "./cursors-presense";
-import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@/lib/utils";
+import {
+  connectionIdToColor,
+  pointerEventToCanvasPoint,
+  resizeBounds,
+} from "@/lib/utils";
 import { nanoid } from "nanoid";
 import { LiveObject } from "@liveblocks/client";
 import { Key } from "lucide-react";
@@ -98,53 +102,63 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     [lastUsedColor]
   );
 
-  const translateSelectedLayer=useMutation((
-    {storage,self},
-    point:Point
-  )=>{
+  const unSelectLayers=useMutation(({self,setMyPresence})=>{
 
-    const offset={
-      x:point.x-canvasState.current.x,
-      y:point.y-canvasState.current.y,
+    if(self.presence.selection.length>0){
+      setMyPresence({
+        selection:[]
+      },{
+        addToHistory:true
+      })
     }
+  },[])
+  const translateSelectedLayer = useMutation(
+    ({ storage, self }, point: Point) => {
+      const offset = {
+        x: point.x - canvasState.current.x,
+        y: point.y - canvasState.current.y,
+      };
 
-    const liveLayers=storage.get("layers");
+      const liveLayers = storage.get("layers");
 
-    for(const id of self.presence.selection){
-      const layer=liveLayers.get(id);
-      if(layer){
-        layer.update({
-          x:layer.get("x")+offset.x,
-          y:layer.get("y")+offset.y,
-        })
+      for (const id of self.presence.selection) {
+        const layer = liveLayers.get(id);
+        if (layer) {
+          layer.update({
+            x: layer.get("x") + offset.x,
+            y: layer.get("y") + offset.y,
+          });
+        }
       }
 
-    }
+      setCanvasState({
+        mode: CanvasMode.Translating,
+        current: point,
+      });
+    },
+    [canvasState]
+  );
 
-    setCanvasState({
-      mode:CanvasMode.Translating,
-      current:point
-    })
-  },[canvasState])
+  const resizeSelectedLayer = useMutation(
+    ({ storage, self }, point: Point) => {
+      if (canvasState.mode != CanvasMode.Resizing) {
+        return;
+      }
 
-  const resizeSelectedLayer = useMutation(({ storage, self }, point: Point) => {
-    if (canvasState.mode != CanvasMode.Resizing) {
-      return;
-    }
+      const bounds = resizeBounds(
+        canvasState.initialBounds,
+        canvasState.corner,
+        point
+      );
 
-    const bounds=resizeBounds(canvasState.initialBounds,
-      canvasState.corner,
-      point
-    )
-
-    const liveLayers=storage.get("layers");
-    const layer=liveLayers.get(self.presence.selection[0]);
-    if(layer){
-      layer.update(bounds);
-    }
-
-
-  }, [canvasState]);
+      const liveLayers = storage.get("layers");
+      const layer = liveLayers.get(self.presence.selection[0]);
+      if (layer) {
+        layer.update(bounds);
+      }
+    },
+    [canvasState]
+  );
 
   const onResizeHandlePointerDown = useCallback(
     (corner: Side, initialBounds: XYWH) => {
@@ -162,7 +176,14 @@ export const Canvas = ({ boardId }: CanvasProps) => {
   const onPointerUp = useMutation(
     ({}, e) => {
       const point = pointerEventToCanvasPoint(e, camera);
-      if (canvasState.mode === CanvasMode.Inserting) {
+      if(canvasState.mode===CanvasMode.None||canvasState.mode===CanvasMode.Pressing){
+        setCanvasState({
+          mode:CanvasMode.None
+        })
+        unSelectLayers();
+
+      }
+      else if (canvasState.mode === CanvasMode.Inserting) {
         insertLayer(canvasState.layerType, point);
       } else {
         setCanvasState({
@@ -172,7 +193,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 
       history.resume();
     },
-    [camera, canvasState, insertLayer, history]
+    [camera, canvasState, insertLayer, history,unSelectLayers]
   );
 
   const onWheel = useCallback((e: React.WheelEvent) => {
@@ -187,17 +208,14 @@ export const Canvas = ({ boardId }: CanvasProps) => {
       e.preventDefault();
 
       const current = pointerEventToCanvasPoint(e, camera);
-      if(canvasState.mode===CanvasMode.Translating){
-
-          translateSelectedLayer(current);
-      }
-      else if (canvasState.mode === CanvasMode.Resizing) {
-  
+      if (canvasState.mode === CanvasMode.Translating) {
+        translateSelectedLayer(current);
+      } else if (canvasState.mode === CanvasMode.Resizing) {
         resizeSelectedLayer(current);
       }
       setMyPresence({ cursor: current });
     },
-    [canvasState,resizeSelectedLayer,camera,translateSelectedLayer]
+    [canvasState, resizeSelectedLayer, camera, translateSelectedLayer]
   );
 
   const onPointerLeave = useMutation(
@@ -206,6 +224,20 @@ export const Canvas = ({ boardId }: CanvasProps) => {
       setMyPresence({ cursor: null });
     },
     []
+  );
+
+  const onPointerDown = useCallback(
+    (e: React.PointerEvent) => {
+      const point = pointerEventToCanvasPoint(e, camera);
+      if (canvasState.mode === CanvasMode.Inserting) {
+        return;
+      }
+
+      ///TODO:add logi for drwaing;
+
+      setCanvasState({ origin: point, mode: CanvasMode.Pressing });
+    },
+    [camera, canvasState.mode, setCanvasState]
   );
 
   const selections = useOthersMapped((other) => other.presence.selection);
@@ -263,6 +295,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         onPointerMove={onPointerMove}
         onPointerLeave={onPointerLeave}
         onPointerUp={onPointerUp}
+        onPointerDown={onPointerDown}
         className="h-[100vh] w-[100vh]"
       >
         <g
